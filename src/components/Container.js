64f9c04498a5be732e7c3cd4229c78127de50fa3
Carina Robinson <carinalrobinson@gmail.com>
2024-02-16 16:48:22 +0100

    remove old v. template paths. add new header/footer content

--- src/components/Container.js
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-const Container = ({ children, page }) => {
-  return <div className={`grid-container ${page ? page : ""}`}>{children}</div>;
-};
-
-export default Container;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <a>Legal Disclosures</a> <span>-</span> <a>Privacy and Cookies</a>
+    </footer>
+  );
+};
+
+export default Footer;
