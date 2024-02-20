2339cbfb4fac744d5c80b65535f61dadb28edd3d
Carina Robinson <carinalrobinson@gmail.com>
2024-02-20 10:31:30 +0100

    add animation v2

--- src/components/Lines.js
+++ src/components/Lines.js
@@ -1,112 +1,83 @@
 import React, { useRef, useEffect, useState } from "react";
+import lineData from "../data/lineData";
 
 const Lines = () => {
   const canvasRef = useRef(null);
-  const [isHovered, setIsHovered] = useState(false);
   const [lines, setLines] = useState([]);
+  const [visibleCount, setVisibleCount] = useState(0);
+
+  useEffect(() => {
+    setLines(lineData.map((line) => ({ ...line, isVisible: false })));
+  }, [setLines]);
+
+  useEffect(() => {
+    const initialLoadInterval = startInitialLoadInterval();
+    const hideRandomLineInterval = startHideRandomLineInterval();
+
+    return () => {
+      clearInterval(initialLoadInterval);
+      clearInterval(hideRandomLineInterval);
+    };
+  }, [lines, visibleCount]);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    drawLines(ctx);
+  }, [lines, visibleCount]);
+
+  const startInitialLoadInterval = () => {
+    return setInterval(() => {
+      setVisibleCount((prevCount) => {
+        if (prevCount < lines.length) {
+          const updatedLines = [...lines];
+          updatedLines[prevCount].isVisible = true;
+          setLines(updatedLines);
+          return prevCount + 1;
+        } else {
+          return prevCount;
+        }
+      });
+    }, 25 - 2 * visibleCount);
+  };
+
+  const startHideRandomLineInterval = () => {
+    return setInterval(() => {
+      setLines((prevLines) => {
+        const updatedLines = prevLines.map((line) => ({
+          ...line,
+          isVisible: true,
+        }));
+
+        const randomIndex = Math.floor(Math.random() * updatedLines.length);
+        updatedLines[randomIndex].isVisible = false;
+
+        return updatedLines;
+      });
+    }, 500);
+  };
 
   const drawLines = (ctx) => {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-    lines.forEach(({ startX, startY, length, numLines }) => {
-      for (let i = 0; i < numLines; i++) {
-        const y = Math.round(startY + i * 5);
-        ctx.beginPath();
-        ctx.moveTo(Math.round(startX), y);
-        const variation = Math.random() * 100;
-        ctx.lineTo(Math.round(startX + length + variation), y);
-        ctx.strokeStyle = "white";
-        ctx.lineWidth = 0.6;
-        ctx.stroke();
-      }
-    });
+    lines
+      .slice(0, visibleCount)
+      .forEach(({ startX, startY, length, isVisible }) => {
+        if (isVisible) {
+          const y = Math.round(startY);
+          ctx.beginPath();
+          ctx.moveTo(startX, y);
+          ctx.lineTo(Math.round(startX + length), y);
+          ctx.strokeStyle = "white";
+          ctx.lineWidth = 0.6;
+          ctx.stroke();
+        }
+      });
   };
-
-  useEffect(() => {
-    const generateLines = () => {
-      const random = Math.random() * 10 + 1;
-
-      return [
-        {
-          startX: 100,
-          startY: 130,
-          length: random + 800,
-          numLines: 5,
-        },
-        {
-          startX: 180,
-          startY: 155,
-          length: random + 500,
-          numLines: 3,
-        },
-        {
-          startX: 60,
-          startY: 170,
-          length: random + 600,
-          numLines: 5,
-        },
-        {
-          startX: 90,
-          startY: 195,
-          length: random + 400,
-          numLines: 5,
-        },
-        {
-          startX: 200,
-          startY: 220,
-          length: random + 500,
-          numLines: 2,
-        },
-        {
-          startX: 0,
-          startY: 230,
-          length: random + 500,
-          numLines: 1,
-        },
-        {
-          startX: 55,
-          startY: 235,
-          length: random + 800,
-          numLines: 5,
-        },
-      ];
-    };
-
-    setLines(generateLines());
-
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-
-    const interval = setInterval(() => {
-      if (isHovered) {
-        setLines(generateLines());
-        drawLines(ctx);
-      }
-    }, 150);
-
-    return () => clearInterval(interval);
-  }, [isHovered]);
-
-  const handleHover = () => {
-    setIsHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    drawLines(ctx);
-  });
 
   return (
-    <div
-      className="canvas-container"
-      onMouseEnter={handleHover}
-      onMouseLeave={handleMouseLeave}
-    >
+    <div className="canvas-container">
       <canvas ref={canvasRef} width={1000} height={400} />
     </div>
   );
---
+++ src/data/lineData.js
@@ -0,0 +1,74 @@
+const lineData = [
+  {
+    startX: 100,
+    startY: 130,
+    length: 800,
+  },
+  {
+    startX: 100,
+    startY: 135,
+    length: 750,
+  },
+  {
+    startX: 100,
+    startY: 140,
+    length: 880,
+  },
+  {
+    startX: 100,
+    startY: 145,
+    length: 820,
+  },
+  {
+    startX: 100,
+    startY: 150,
+    length: 900,
+  },
+  {
+    startX: 180,
+    startY: 155,
+    length: 500,
+  },
+  {
+    startX: 180,
+    startY: 160,
+    length: 504,
+  },
+  {
+    startX: 180,
+    startY: 165,
+    length: 520,
+  },
+  { startX: 60, startY: 170, length: 616 },
+  { startX: 60, startY: 175, length: 600 },
+  { startX: 60, startY: 180, length: 604 },
+  { startX: 60, startY: 185, length: 612 },
+  { startX: 60, startY: 190, length: 624 },
+  { startX: 60, startY: 195, length: 600 },
+  { startX: 90, startY: 200, length: 400 },
+  { startX: 90, startY: 205, length: 404 },
+  {
+    startX: 200,
+    startY: 210,
+    length: 512,
+  },
+  {
+    startX: 200,
+    startY: 215,
+    length: 508,
+  },
+  {
+    startX: 200,
+    startY: 220,
+    length: 516,
+  },
+  { startX: 0, startY: 225, length: 600 },
+  { startX: 55, startY: 230, length: 804 },
+  { startX: 55, startY: 235, length: 812 },
+  { startX: 55, startY: 240, length: 824 },
+  { startX: 55, startY: 245, length: 604 },
+  { startX: 55, startY: 250, length: 616 },
+  { startX: 55, startY: 255, length: 402 },
+];
+
+export default lineData;
