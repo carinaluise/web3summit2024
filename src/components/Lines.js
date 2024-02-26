e9572ada4fa774ec1178e37c3d48a7d563e0ef3a
Carina Robinson <carinalrobinson@gmail.com>
2024-02-26 17:43:07 +0100

    add scroll/cursor animation

--- src/components/Lines.js
+++ src/components/Lines.js
@@ -1,32 +1,97 @@
-import React, { useEffect } from "react";
-import { motion, useMotionValue, useTransform } from "framer-motion";
-import linesSVG from "../images/lines.svg";
+import React, { useEffect, useState } from "react";
+import { motion, useMotionValue, useTransform, useScroll } from "framer-motion";
+import Frame1 from "../images/frame-1.svg";
+import Frame2 from "../images/frame-2.svg";
 
 const Lines = () => {
-  const x = useMotionValue(200);
-  const y = useMotionValue(200);
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const { scrollYProgress } = useScroll();
+  const [isMobile, setIsMobile] = useState(false);
+
+  const isWindowDefined = typeof window !== "undefined";
 
   useEffect(() => {
-    const handleMouseMove = (e) => {
-      x.set(e.pageX);
-      y.set(e.pageY);
-    };
+    if (isWindowDefined) {
+      setIsMobile(window.innerWidth <= 790);
 
-    document.addEventListener("mousemove", handleMouseMove);
+      const handleMouseMove = (e) => {
+        x.set(e.pageX);
+        y.set(e.pageY);
+      };
 
-    return () => {
-      document.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, [x, y]);
+      const handleResize = () => {
+        setIsMobile(window.innerWidth <= 790);
+        if (isMobile) {
+          x.set(0);
+          y.set(0);
+        }
+      };
 
-  const rotateX = useTransform(y, [0, 400], [-45, 45]);
-  const rotateY = useTransform(x, [0, 400], [-5, 5]);
+      const addOrRemoveEventListener = () => {
+        if (isMobile) {
+          document.removeEventListener("mousemove", handleMouseMove);
+        } else {
+          document.addEventListener("mousemove", handleMouseMove);
+        }
+      };
+
+      addOrRemoveEventListener();
+      window.addEventListener("resize", handleResize);
+
+      return () => {
+        document.removeEventListener("mousemove", handleMouseMove);
+        window.removeEventListener("resize", handleResize);
+      };
+    }
+  }, [isMobile, x, y, isWindowDefined]);
+
+  const translateYLayer1 = useTransform(
+    y,
+    [
+      0,
+      isWindowDefined ? window.innerHeight / 2 : 0,
+      isWindowDefined ? window.innerHeight - 100 : 0,
+    ],
+    [0, 10, 0]
+  );
+
+  const translateYLayer2 = useTransform(
+    y,
+    [
+      0,
+      isWindowDefined ? window.innerHeight / 2 : 0,
+      isWindowDefined ? window.innerHeight - 100 : 0,
+    ],
+    [0, -10, 0]
+  );
+
+  const translateYLayer1Scroll = useTransform(
+    scrollYProgress,
+    [0.1, 0.5],
+    [0, -5]
+  );
+
+  const translateYLayer2Scroll = useTransform(
+    scrollYProgress,
+    [0.1, 0.5],
+    [0, 10]
+  );
 
   return (
     <div className="svg-container">
-      <motion.img style={{ rotateX, rotateY }} src={linesSVG} alt="" />
-      <motion.img src={linesSVG} alt="" />
-      <motion.img src={linesSVG} alt="" />
+      <motion.img
+        src={Frame1}
+        style={{
+          y: isMobile ? translateYLayer1Scroll : translateYLayer1,
+        }}
+      />
+      <motion.img
+        src={Frame2}
+        style={{
+          y: isMobile ? translateYLayer2Scroll : translateYLayer2,
+        }}
+      />
     </div>
   );
 };
