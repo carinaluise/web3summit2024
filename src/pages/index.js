efd124c0fb736711e06a8cb3d3412ad3c69997de
Carina Robinson <carinalrobinson@gmail.com>
2024-02-28 14:54:24 +0100

    implement modal and styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Layout from "../components/Layout";
 import Lines from "../components/Lines";
+import Modal from "../components/Modal";
 
 const HomePage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
   return (
-    <Layout className="home" title="Homepage">
+    <Layout className="home">
       <div className="home__hero">
         <Lines />
         <div className="home__title-container">
@@ -15,13 +22,21 @@ const HomePage = () => {
       </div>
       <article className="home__cta">
         <p>
-          <a href="/">Sign up</a>
+          <button onClick={openModal}>Sign up</button>
           for updates
         </p>
         <p>
-          <a href="/">Submit</a>a proposal
+          <a
+            target="_blank"
+            rel="noreferrer noopenner"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSe5X9pYNbSKFBJ-Rpis9Li4xqLnlIN1-iHNDOWAwb03qhx6_w/viewform?usp=sf_link"
+          >
+            Submit
+          </a>
+          a proposal
         </p>
       </article>
+      <Modal isOpen={isOpen} closeModal={() => setIsOpen(false)} />
     </Layout>
   );
 };
