1362000ec45619ba3a27524cee95180d4605b1a8
rmnprkrl <rmnprkrl@gmail.com>
2024-04-16 09:14:32 +0200

    update style

--- src/pages/volunteer.js
+++ src/pages/volunteer.js
@@ -8,18 +8,16 @@ const VolunteerPage = () => {
 
   return (
     <Layout page="volunteer">
-      <div className="page-header">
-        <h2>Volunteer</h2>
-        <p>
-          Are you interested in actively participating and delving deeper into
-          the world of Web3 at the Web3 Summit? <br />
+      <p>
+        Are you interested in actively participating and delving deeper into the
+        world of Web3 at the Web3 Summit? <br />
+        <span className="text-dark">
           If so, volunteering at this year's edition is the perfect fit for you!{" "}
           <br />
           You’ll spend three days collaborating, coordinating, and gaining
           insights into the inner workings of organizing a congress.
-        </p>
-      </div>
-
+        </span>
+      </p>
       <p>
         To volunteer, you must fulfil the following requirements:
         <br />
@@ -34,8 +32,9 @@ const VolunteerPage = () => {
         In addition to an incredible experience, volunteers will benefit from:
         <br />
         <span className="text-dark">
-          - Free entry to the conference Complimentary food & drinks on the day
-          of your assignment(s)
+          - Free entry to the conference
+          <br />
+          - Complimentary food & drinks on the day of your assignment(s)
           <br />- A Web3 Summit T-shirt
         </span>
       </p>
@@ -58,7 +57,7 @@ const VolunteerPage = () => {
         <span className="text-dark">May 17, 2024</span>
       </p>
 
-      <p className="text-dark">
+      <p>
         We thank you in advance for your assistance at the Summit and are
         looking forward to seeing you there for three unforgettable days!{" "}
       </p>
